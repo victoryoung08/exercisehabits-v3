cf0fc2443872bf3c0db5323b46a0562319d7d570
victoryoung <vicmachine08@gmail.com>
2024-04-29 16:35:33 +1000

    config domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: [
+      "localhost",
+      "exercise-habits-backend-production.up.railway.app",
+      "exercisehabits-v3.vercel.app",
+    ],
     remotePatterns: [
       {
         protocol: "http",
@@ -10,6 +15,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "exercise-habits-backend-production.up.railway.app",
+        port: '4000',
       },
       {
         protocol: "https",
